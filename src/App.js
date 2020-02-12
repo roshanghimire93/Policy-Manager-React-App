a2a4714033a93a73fe9728d4f93b75aa4bf677d6
rghimire <rghimire@cs.odu.edu>
2020-02-12 01:15:33 -0500

    Added loading message and logo

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import React, { Component } from 'react';
 import './App.css';
+import logo from './logo.svg'
 import PoliciesArray from './PoliciesArray';
 import 'tachyons';
 import SearchBox from './SearchBox';
@@ -31,13 +32,23 @@ class App extends Component {
     const filteredlist = this.state.PoliciesList.filter(policy => {
       return policy.title.toLowerCase().includes(this.state.searchfield.toLowerCase())
     })
-    return (
-      <div className="App">
-        <h1 className='Title'>POLICIES</h1>
-        <SearchBox searchChange = {this.onSearchChange}/>
-        <PoliciesArray PoliciesList = {filteredlist}/>
-      </div>
-    );
+    if(this.state.PoliciesList.length === 0){
+      return(
+        <div className='tc'>
+          <img className='App-logo' src={logo} alt='logo'/>
+          <h1>Loading..</h1>
+        </div>
+      )
+    }
+    else{
+      return (
+        <div className='App'>
+          <h1 className='Title'>POLICIES</h1>
+          <SearchBox searchChange = {this.onSearchChange}/>
+          <PoliciesArray PoliciesList = {filteredlist}/>
+        </div>
+      );
+    }
   }
 }
 
