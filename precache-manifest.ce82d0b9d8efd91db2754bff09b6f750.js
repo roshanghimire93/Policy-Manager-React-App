1f25872e5cf06edc0b2eb90fb3fd3fcbdd63d8fd
rghimire <rghimire@cs.odu.edu>
2020-02-13 21:03:48 -0500

    Updates

---
+++ precache-manifest.ce82d0b9d8efd91db2754bff09b6f750.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ae21b0205cdbf62e21683d3a1f8f0ee1",
+    "url": "/Policy-Manager-React-App/index.html"
+  },
+  {
+    "revision": "449caa3afc34fd1f0417",
+    "url": "/Policy-Manager-React-App/static/css/2.eb45ad44.chunk.css"
+  },
+  {
+    "revision": "e6b4debbfd4865471afb",
+    "url": "/Policy-Manager-React-App/static/css/main.27d5ed80.chunk.css"
+  },
+  {
+    "revision": "449caa3afc34fd1f0417",
+    "url": "/Policy-Manager-React-App/static/js/2.95dfed40.chunk.js"
+  },
+  {
+    "revision": "99bd0487192ec9e7d9ee8fbbd91ee444",
+    "url": "/Policy-Manager-React-App/static/js/2.95dfed40.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "e6b4debbfd4865471afb",
+    "url": "/Policy-Manager-React-App/static/js/main.a5e53ed4.chunk.js"
+  },
+  {
+    "revision": "656cf7fd39e499be55d4",
+    "url": "/Policy-Manager-React-App/static/js/runtime-main.5b5852f9.js"
+  },
+  {
+    "revision": "fca235345fc5f0b9c0bd0c5e05b0c793",
+    "url": "/Policy-Manager-React-App/static/media/Gameplay.fca23534.woff"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/Policy-Manager-React-App/static/media/logo.5d5d9eef.svg"
+  }
+]);
